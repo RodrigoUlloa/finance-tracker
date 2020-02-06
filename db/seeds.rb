389c2b174e9001e6d5395b85b24f2dd149bac77a
Rodrigo Ulloa <rodrigo.ulloa22@gmail.com>
2020-02-06 14:03:17 -0300

    added api key, and model intermediary  user_stock

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+5.times do |i|
+  u = User.create!(email: "email#{i}@gmail.com", password: "123456")
+end
